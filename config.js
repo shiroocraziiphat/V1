39aea8c25413f16c1d2c919451334dc076555a10
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-02-09 04:59:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ['6282361160044', '6285212023766'] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -30,10 +30,10 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'arif️fb'
-global.author = 'amel'
+global.packname = 'shiro-CP'
+global.author = 'lovyuu<3'
 
-global.wm = '© ariffb & melcanz'
+global.wm = '©shiro_craziiphat'
 global.wait = '_*tunggu sedang diproses...*_'
 global.eror = '_*Server Error*_'
 global.benar = '✅'
